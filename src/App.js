22bec8a09cbb680db81330eaeb1c8ebb6c26db9e
angelina <zhuravskayalina@gmail.com>
2022-08-20 00:36:02 +0400

    learning components

--- src/App.js
+++ src/App.js
@@ -4,9 +4,37 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <h1>Hi Everyone!</h1>
-      
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>I am header!</h1>
+      
+    </header>
+  );
+}
+
+const Main = () => {
+  return (
+    <main className="main">
+      <h1>I am main section!</h1>
+      
+    </main>
+  );
+}
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h1>I am footer!</h1>
+      
+    </footer>
   );
 }
 

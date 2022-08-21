111fed7485e05eb363eb6f8a33e219f221925ed0
angelina <zhuravskayalina@gmail.com>
2022-08-21 19:03:16 +0400

    start social media project

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header.js';
+
 
 
 const App = () => {
   return (
-    <div className="App">
-      <Header/>
-      <Main/>
-      <Footer />
+    <div className = 'app-wrapper'>
+     <header className='header'>
+        <img className="logo-img" src="https://img.freepik.com/free-vector/cute-alien-hug-ufo-toy-cartoon-vector-icon-illustration-science-technology-icon-concept-isolated_138676-5307.jpg?w=1060&t=st=1661092476~exp=1661093076~hmac=66a37d8e62c3473f28578ccd61f121ec9465920412bbc1b67dc092f6d8a37828" alt="logo - alien" title="alien logo"></img>
+     </header>
+     <nav className='nav'>
+        <div>Profile</div>
+        <div>Messages</div>
+        <div>Profile</div>
+     </nav>
+     <main className='main'>
+      main content 
+     </main>
+
     </div>
   );
-}
-
-
-const Item = () => {
-  return (
-    <li>
-      hi
-    </li>
-  );
-}
-
-const Main = () => {
-  return (
-    <main className="main">
-      <h1>I am main section!</h1>
-      
-    </main>
-  );
-}
-
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <h1>I am footer!</h1>
-      
-    </footer>
-  );
-}
+};
 
 export default App;
+

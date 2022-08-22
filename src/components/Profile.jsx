6238cac8b296c8679600a52de2c61a27257c9d33
angelina <zhuravskayalina@gmail.com>
2022-08-22 22:02:56 +0400

    feat: main section cover maket is done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,14 +3,14 @@ import classes from './Profile.module.css';
 const Profile = () => {
   return (
     <main className={classes.main}>
-      <img className={classes['main-img']} src="https://assets.vogue.ru/photos/5f3e2706a58151d7dfc76773/16:9/w_2580%2Cc_limit/Britney01.jpg" alt="britney image"></img>
-      <img className={classes.avatar} src='https://upload.wikimedia.org/wikipedia/commons/b/bc/Juvenile_Ragdoll.jpg' alt='avatar'></img>
-      <div className={classes['main-content']}>
-        <div className={classes.posts}>
-          <div className={classes.item}>Post 1</div>
-          <div className={classes.item}>Post 2</div> 
+      <div className={classes.wrapper}>
+        <div className={classes.cover}>
+          <div className={classes.profilePhoto}></div>
+          <div className={classes.coverInfo}>
+            <p className={classes.profileName}>Kat Kat</p>
+            <p className={classes.profileStatus}>hi everyone</p>
+          </div>
         </div>
-
       </div>
     </main>
   )

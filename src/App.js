0008af9456bcc1a21543a4adee8ed451a3c0553f
angelina <zhuravskayalina@gmail.com>
2022-08-23 16:35:46 +0400

    props learning

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navigation from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-
+import Dialogs from "./components/Dialogs/Dialogs";
 
 
 const App = () => {
-  return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navigation/>
-      <Profile name="Britney"/>
-      
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navigation/>
+
+            {/*<Profile name="Britney"/>*/}
+            <Dialogs/>
+
+        </div>
+    );
 };
 
 export default App;
---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,11 @@
+import classes from './Dialogs.module.css';
+
+const Dialogs = () => {
+    return (
+        <div className={classes.wrap}>
+
+        </div>
+    )
+}
+
+export default Dialogs;
